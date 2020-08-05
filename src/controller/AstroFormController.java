1a246aa4d1ec30af9d55a196f55a2e66424a248d
saikumar22 <saigoud2212@gmail.com>
2020-08-05 16:26:33 +0530

    FSJ0720A116

--- src/controller/AstroFormController.java
+++ src/controller/AstroFormController.java
@@ -1,4 +1,3 @@
-
 package controller;
 
 import java.io.IOException;
@@ -9,10 +8,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.User;
+import service.AstroCalculator;
 
 @WebServlet(urlPatterns = {"/entervalue"})
 public class AstroFormController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/entervalues.jsp");
@@ -20,13 +22,18 @@ public class AstroFormController extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-/* 
+
 		String name=request.getParameter("name");
 		String gender=request.getParameter("gender");
 		String date=request.getParameter("dob1");
 		String month=request.getParameter("dob2");
 		String year=request.getParameter("dob3");
-		String dob = date+"-"+month+"-"+year;	
+		String dob = date+"-"+month+"-"+year;
+		
+		User  u=new User(name,date,month,gender);
+		AstroCalculator ac = new AstroCalculator();
+		String astrosign = ac.findSign(u);
+		System.out.println(astrosign);
 		
 		if(astrosign.equals("Aquarius")){
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
@@ -88,6 +95,6 @@ public class AstroFormController extends HttpServlet {
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/virgo.svg'>");
 			rd.forward(request, response);
-		}*/
+		}
 	}
 }
--- src/model/User.java
+++ src/model/User.java
@@ -1,3 +1,16 @@
 package model;
-
 //Type your code here
+public class User{
+	public String name;
+	 public int d;
+	public int m;
+	
+
+	public User(String name,String date,String month,String gender)
+	{
+		this.name=name;
+		d=Integer.parseInt(date);
+		m=Integer.parseInt(month);
+	}
+	
+}
